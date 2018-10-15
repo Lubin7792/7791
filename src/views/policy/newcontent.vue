03a530d13b10795227c81ac8361337dd92980415
PatelleLu <471569318@qq.com>
2018-10-15 22:36:29 +0800

    day

--- src/views/policy/newcontent.vue
+++ src/views/policy/newcontent.vue
@@ -3,115 +3,120 @@
 </style>
 
 <template>
-	<div class="newconten">
-		<div v-show="show==='基本信息'"  class="basicinfo">
-			<Form ref="basic" :model="basic" :label-width="80">
-				<FormItem label="策略名称">
-					<Input v-model="basic.name"></Input>
-				</FormItem>
-				<FormItem label="策略类型">
-					<Input v-model="basic.type"></Input>
-				</FormItem>
-				<FormItem label="储存设备">
-					<Input v-model="basic.client"></Input>
-				</FormItem>
-				<FormItem label="介质池">
-					<Input v-model="basic.state"></Input>
-				</FormItem>
-        	<FormItem label="优先级">
-					<Input v-model="basic.state"></Input>
-				</FormItem>
-        	<FormItem label="策略最大调度任务">
-					<Input v-model="basic.state"></Input>
-				</FormItem>
-        	<FormItem label="启用压缩">
-					<Input v-model="basic.state"></Input>
-				</FormItem>
-        	<FormItem label="启用加密">
-					<Input v-model="basic.state"></Input>
-				</FormItem>
-        	<FormItem label="加密算法">
-					<Input v-model="basic.state"></Input>
-				</FormItem>
-        	<FormItem label="数据保留周期">
-					<Input v-model="basic.state"></Input>
-				</FormItem>
-			</Form>
-		</div>
-		<div v-show="show==='备份选项'">
-      <Tabs type="card" :animated="false"   @on-click="click2">
-     	<TabPane v-for="(tab,index) in tabList" :label="tab.title" :name="tab.title" :key="index">
-         <backupoption :show2="show2"></backupoption> 
-      </TabPane>
-    </Tabs>  
-		</div>
-		<div v-show="show==='备份资源列表'">
-			<Form ref="option" :model="option" :label-width="80">
-				<FormItem label="备份内容">
-					<Input v-model="option.content"></Input>
-				</FormItem>
-				<FormItem label="启用加密">
-					<Input v-model="option.encryption"></Input>
-				</FormItem>
-				<FormItem label="启用压缩">
-					<Input v-model="option.compress"></Input>
-				</FormItem>
-			</Form>
-		</div>
-		<div v-show="show==='调度计划'" class="planinfo">
-			<Form ref="plan" :model="plan" :label-width="80">
-          	<FormItem label="调度类型">
-					<Select v-model="plan1" style="width:112px" @on-change="plantype">
-						<Option v-for="item in planty" :value="item.value" :key="item.value"></Option>
-					</Select>
-				</FormItem>
-        	<FormItem label="备份类型">
-					<Select v-model="plan1" style="width:112px" @on-change="plantype">
-						<Option v-for="item in planty" :value="item.value" :key="item.value"></Option>
-					</Select>
-				</FormItem>
-			<div  v-show="show3==='月份'">
-        	<FormItem label="开始时间">
-					<TimePicker format="HH:mm" placeholder="Select time" style="width: 112px"></TimePicker>
-				</FormItem>
-        		<FormItem label="结束时间">
-					<TimePicker format="HH:mm" placeholder="Select time" style="width: 112px"></TimePicker>
-				</FormItem>
-				<FormItem label="间隔时间">
-					<Select v-model="model1" style="width:200px">
-						<Option v-for="item in frequency" :value="item.value" :key="item.value">{{ item.label }}</Option>
-					</Select>
-				</FormItem>
-      </div>
-      	<div  v-show="show3==='周份'">
-        	<FormItem label="开始时间">
-					<TimePicker format="HH:mm" placeholder="Select time" style="width: 112px"></TimePicker>
-				</FormItem>
-      </div>
-      <div  v-show="show3==='小时'">
-        	<FormItem label="开始时间">
-					<TimePicker format="HH:mm" placeholder="Select time" style="width: 112px"></TimePicker>
-				</FormItem>
-        		<FormItem label="结束时间">
-					<TimePicker format="HH:mm" placeholder="Select time" style="width: 112px"></TimePicker>
-				</FormItem>
-				<FormItem label="间隔时间">
-					<Select v-model="model1" style="width:200px">
-						<Option v-for="item in frequency" :value="item.value" :key="item.value">{{ item.label }}</Option>
-					</Select>
-				</FormItem>
-      </div>
-			</Form>
+  <div class="newconten">
+    <div v-show="show==='基本信息'" class="basicinfo">
+      <Form ref="basic" :model="basic" :label-width="80">
+        <FormItem label="策略名称">
+          <Input v-model="basic.name"></Input>
+        </FormItem>
+        <FormItem label="策略类型">
+          <Input v-model="basic.type"></Input>
+        </FormItem>
+        <FormItem label="储存设备">
+          <Input v-model="basic.client"></Input>
+        </FormItem>
+        <FormItem label="介质池">
+          <Input v-model="basic.state"></Input>
+        </FormItem>
+        <FormItem label="优先级">
+          <Input v-model="basic.state"></Input>
+        </FormItem>
+        <FormItem label="策略最大调度任务">
+          <Input v-model="basic.state"></Input>
+        </FormItem>
+        <FormItem label="启用压缩">
+          <Input v-model="basic.state"></Input>
+        </FormItem>
+        <FormItem label="启用加密">
+          <Input v-model="basic.state"></Input>
+        </FormItem>
+        <FormItem label="加密算法">
+          <Input v-model="basic.state"></Input>
+        </FormItem>
+        <FormItem label="数据保留周期">
+          <Input v-model="basic.state"></Input>
+        </FormItem>
+      </Form>
+    </div>
+    <div v-show="show==='备份选项'">
+      <Tabs type="card" :animated="false" @on-click="click2">
+        <TabPane v-for="(tab,index) in tabList" :label="tab.title" :name="tab.title" :key="index">
+          <backupoption :show2="show2"></backupoption>
+        </TabPane>
+      </Tabs>
+    </div>
+    <div v-show="show==='备份资源列表'">
+      <Form ref="option" :model="option" :label-width="80">
+        <FormItem label="备份内容">
+          <Input v-model="option.content"></Input>
+        </FormItem>
+        <FormItem label="启用加密">
+          <Input v-model="option.encryption"></Input>
+        </FormItem>
+        <FormItem label="启用压缩">
+          <Input v-model="option.compress"></Input>
+        </FormItem>
+      </Form>
+    </div>
+    <div v-show="show==='调度计划'" class="planinfo">
+      <Form ref="plan" :model="plan" :label-width="80">
+        <FormItem label="调度类型">
+          <Select v-model="plan1" style="width:112px" @on-change="plantype">
+            <Option v-for="item in planty" :value="item.value" :key="item.value"></Option>
+          </Select>
+        </FormItem>
+        <FormItem label="备份类型">
+          <Select v-model="plan1" style="width:112px" @on-change="plantype">
+            <Option v-for="item in planty" :value="item.value" :key="item.value"></Option>
+          </Select>
+        </FormItem>
+        <div v-show="show3==='月份'">
+          <FormItem label="开始时间">
+            <DatePicker type="daterange" show-week-numbers placement="bottom-end" placeholder="Select date" style="width: 410px"></DatePicker>
+          </FormItem>
+        </div>
+        <div v-show="show3==='周'">
+          <FormItem label="开始时间">
+            <TimePicker format="HH:mm" placeholder="Select time" style="width: 112px"></TimePicker>
+          </FormItem>
+          <FormItem label="结束时间">
+            <TimePicker format="HH:mm" placeholder="Select time" style="width: 112px"></TimePicker>
+          </FormItem>
+          <FormItem label="间隔时间">
+            <Select v-model="model1" style="width:200px">
+              <Option v-for="item in frequency" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+          </FormItem>
+        </div>
+        <div v-show="show3==='天数'">
+          <FormItem label="开始时间">
+            <DatePicker type="daterange" show-week-numbers placement="bottom-end" placeholder="Select date" style="width: 410px"></DatePicker>
+          </FormItem>
+        </div>
+        <div v-show="show3==='小时'">
+          <FormItem label="开始时间">
+            <TimePicker format="HH:mm" placeholder="Select time" style="width: 112px"></TimePicker>
+          </FormItem>
+          <FormItem label="结束时间">
+            <TimePicker format="HH:mm" placeholder="Select time" style="width: 112px"></TimePicker>
+          </FormItem>
+          <FormItem label="间隔时间">
+            <Select v-model="model1" style="width:200px">
+              <Option v-for="item in frequency" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+          </FormItem>
+        </div>
+      </Form>
       <div class="button">
         <Button type="warning">添加计划</Button>
         <Button type="warning">保存计划</Button>
         <Button type="warning">删除计划</Button>
       </div>
       <div class="planlist">
-       <Table border :columns="planlist" :data="planlistcon" ></Table>
+        <Table border :columns="planlist" :data="planlistcon"></Table>
       </div>
-		</div>
-	</div>
+    </div>
+  </div>
 </template>
 <script>
 import backupoption from "./backupoption";
@@ -135,10 +140,10 @@ export default {
         value:"月份"
       },
         {
-        value:"周份"
+        value:"周"
       },
         {
-        value:"天"
+        value:"天数"
       },
         {
         value:"小时"
